61374314f850a025a78ca667431a57322b623a77
Niki Ito <niki.art.ito@gmail.com>
2021-07-23 19:32:30 -0400

    Update p5 canvas

--- script/visual-narrative/hatsue&niki/hatsue&niki.js
+++ script/visual-narrative/hatsue&niki/hatsue&niki.js
@@ -3,33 +3,44 @@ let img;
 
 function preload() {
   myAudio = loadSound("../assets/audio/visual-narrative/smells.mp3");
-  img = loadImage("../assets/images/visual-narrative/placeholder/placeholder_3.png");
+  img = loadImage("../assets/images/visual-narrative/hatsue&niki/hatsue-10.jpg");
 }
 
 function setup() {
-  createCanvas(800, 800);
-  fft = new p5.FFT(0.9, 64);
+  canvas = createCanvas(800, 800);
+  canvas.style("width", "90%");
+  canvas.style("height", "auto");
+  canvas.style("margin", "0 auto");
+  canvas.parent("canvas");
+  
+  subtitle = select("h2");
+  
+  fft = new p5.FFT(0.9,64);
   myAudio.setVolume(1);
   
-  button = createButton("play");
-  button.mousePressed(togglePlaying);
-  button.position(0,0)
-  
+  //see button.js
+  drawButtons();
+  //see subtitles.js
+  incertText();
+  incertCue();
+
   for (let i=1; i<5; i++){
     document.getElementById("clickable-"+i).addEventListener("click", showCanvas);
   }
+
 }
 
 function showCanvas(){
-    document.getElementById("defaultCanvas0").style.visibility="visible";
-    console.log("clicked")
+  document.getElementById("defaultCanvas0").style.visibility="visible";
+  console.log("clicked")
 }
 
 function draw() {
   background(255);
 
-  timer();
+  image(img, 0, 0, width, height);
 
+  //audio visualizer
   let spectrum = fft.analyze();
   noStroke();
 
@@ -37,28 +48,17 @@ function draw() {
     let x = map(i, 0, spectrum.length, 0, width);
     let h = map(spectrum[i], 0, 255, 0, height);
     fill(spectrum[i] * 1.5 + 50, 173, 159, 80);
-    // rect(x,height-h, width/spectrum.length-2, h);
-
     rect(x, 0, width / spectrum.length, height);
   }
-
-  blend(img, 0, 0, width, height, 0, 0, width, height, MULTIPLY);
-}
-
-function togglePlaying(){
-  if (!myAudio.isPlaying()){
-    myAudio.play();
-    button.html("pause");
-  } else {
-    myAudio.pause();
-    button.html("play");
-  }
   
+  timer();
+  timeSubtitles();
+  showText();
 }
 
 function timer() {
   let counter = myAudio.currentTime();
   let barWidth = width/myAudio.duration();
-  
+  // console.log(myAudio.currentTime())
   rect(0,height-20,barWidth*counter,20)
 }
