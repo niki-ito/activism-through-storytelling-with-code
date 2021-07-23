f040850f59c286b352a2c1df12a8bf1692908aba
Niki Ito <niki.art.ito@gmail.com>
2021-07-23 16:43:17 -0400

    Update example-4

---
+++ script/example-4/button.js
@@ -0,0 +1,28 @@
+function drawButtons() {
+    button = createButton("&#9654");
+    button.mousePressed(togglePlaying);
+    button.position(340,670);
+    button.style("background-color:black");
+    button.style("color: grey");
+    button.style("border: 0px");
+    button.style("font-size:1.4rem");
+    
+    buttonX = createButton("&#10006");
+    buttonX.position(665,5);
+    buttonX.style("background-color:black");
+    buttonX.style("color: grey");
+    buttonX.style("border: 0px");
+    buttonX.style("font-size:1.4rem");
+    
+  }
+  
+  function togglePlaying(){
+    if (!myAudio.isPlaying()){
+      myAudio.play();
+      button.html("&#10074&#10074");
+    } else {
+      myAudio.pause();
+      button.html("&#9654");
+    }
+    
+  }
---
+++ script/example-4/subtitles.js
@@ -0,0 +1,34 @@
+let subtitleArray = [];
+let subtitleCueArray = [];
+let index = -1;
+
+//Show appropriate subtitle using the recorded time stamps 
+function timeSubtitles() {
+  for (let i = 0; i < subtitleCueArray.length; i ++){
+    if (myAudio.currentTime()>=subtitleCueArray[i]){
+      index = i;
+    } 
+  }
+}
+
+//Tool used to create time staps for the subtitles
+
+// function mouseClicked() {
+//   index = index + 1
+//   console.log(myAudio.currentTime());
+// }
+
+function showText() {
+  subtitle.html(subtitleArray[index]);
+}
+
+function incertText() {
+  subtitleArray.push("So I’m very attached to smells", "and so there’s a lot of specific smells", "that I associate <br>with many different things.", "Like the bathroom in the beginning", "it kind of smelled like<br> my grandma’s bathroom", "and I’ll get really nostalgic about that.", "The smell of right<br> when you open <i>yakuruto</i>", "a specific memory in <br>my grandma’s apartment", "late at night, <br>they used to have", "a refrigerator next<br> to the toilet room", "and we would just walk down", "and I remember this specific feeling <br>of the wooden floors", "and her getting a <i>yakuruto</i> out <br>of the refrigerator for me", "and that smell, the wood floors", "also the cold refrigerator<br> and the opening of it.", "Ugh...", "And then we have blankets<br> at my<i> house</i> house", "that we have vacuum packed<br> just to keep for storage", "and they kind of still smell like<br> my grandma’s house of incense", "so I’m always smelling them.","Wow.<br> So your grandma that lives in Japan?","Yeah, in Japan.","Huh, is the incense from a…","I forget what you call it.","<i>Butsudan? Butsudan</i>. Ohh.","Yeah.","My grandma’s place has a butsudan too", "because my grandfather passed away<br> when I was born", "so we’ve always had a <i>butsudan</i><br> at my grandma’s place", "and we wouldn’t really have incense", "but there was a little bell <br>that we would always ring", "when we want to say something", "and then we would always give food.","Yeah.","So my grandma<br> passed away last summer.","In Japan? Yes.","So now they’re both in the shrine.","But my mom’s older sister", "they live in Saitama", "and so my grandma’s apartment<br> is in Yokosuka", "so they’re actually there now.","So they still kind of<br> go to that apartment.","They still have the apartment. ");
+}
+
+//Time stamp copied and pasted from console
+function incertCue() {
+subtitleCueArray.push(
+0,2.3684126984126985,4.969047619047619,7.865736961451248,10.419931972789115,12.718707482993198,14.378934240362812,19.06936507936508,22.66845804988662,25.164603174603176,28.66501133786848,30.476167800453513,32.9781179138322,36.670090702947846,38.87018140589569,43.06718820861678,44.51843537414966,49.06374149659864,53.86446712018141,56.72052154195011,58.71743764172336,62.16560090702948,63.66909297052154,67.4191156462585,68.42918367346938,71.2678231292517,72.27789115646259,75.01784580498867,78.16995464852607,81.76904761904763,85.61775510204082,89.91925170068028,93.11780045351473,94.61548752834467,95.51526077097506,98.51643990929705,101.51181405895692,106.56795918367347,108.5242403628118,110.61984126984127,114.22473922902495,116.01267573696146,118.6191156462585
+)
+}
